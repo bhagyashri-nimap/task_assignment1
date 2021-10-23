2cd4afa8998a702d137dc23c6ba35b65bbbe26cd
bhagyashri3 <bhagyashri@nimapinfotech.com>
2021-10-23 14:09:38 +0530

    added changes

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -2,22 +2,7 @@
 var { messageData } = require('../mongooseModel/Message.js');
 var { collectionData } = require('../mongooseModel/Collection2.js');
 var moment=require('moment');
- function timeStamps (data) {
-     console.log("data")
-    let saveMsg
-    let newObj = {
-     message: data
-    }
-    let Obj = new collectionData(newObj)
-    saveMsg = Obj.save()
-    // if (saveMsg && !saveMsg._id) {
-    //     return {
-    //         data: "Something Went Wrong While Saving data Message",
-    //         value: false
-    //     }
-    // }
-   
-}
+ 
 exports.save = async function (data) {
     var date = new Date(data.day);
     var unixTimeStamp = Math.floor(date.getTime() / 1000);
@@ -37,10 +22,9 @@ exports.save = async function (data) {
             }
         }else{
             var startDate = new Date();
-            console.log(startDate,"startDate")
             var endDate = data.day+"T"+data.time
-            var date = moment(endDate).utcOffset('+05:30').format('YYYY-MM-DD HH:mm:ss');
-            var seconds = (new Date(date).getTime() - startDate.getTime());
+            var formateDate = moment(endDate).utcOffset('+05:30').format('YYYY-MM-DD HH:mm:ss');
+            var seconds = (new Date(formateDate).getTime() - startDate.getTime());
             var intervel= setInterval(function timeStamps() {
                 let newObj = {
                     message: data.message
