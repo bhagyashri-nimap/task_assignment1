7e672ebbca2bf4648eeaf3de4528ae0281be7c80
bhagyashri3 <bhagyashri@nimapinfotech.com>
2021-10-23 14:26:42 +0530

    added changes

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -4,14 +4,19 @@ var { collectionData } = require('../mongooseModel/Collection2.js');
 var moment=require('moment');
  
 exports.save = async function (data) {
-    var date = new Date(data.day);
-    var unixTimeStamp = Math.floor(date.getTime() / 1000);
-    var date1= new Date(unixTimeStamp*1000);
-    var finaldate = date1.toLocaleDateString("en-US")
+    var startDate = new Date();
+    var endDate = data.day+"T"+data.time
+    var formateDate = moment(endDate).utcOffset('+05:30').format('YYYY-MM-DD HH:mm:ss');
+    var seconds = (new Date(formateDate).getTime() - startDate.getTime());
+     if(startDate>new Date(formateDate)){
+        return{
+            data:"invalid date",
+            value:true} 
+        }
     var savemessage
        let newObj2 = {
         message: data.message,
-        msgTimestamps:new Date(finaldate+" "+data.time)
+        msgTimestamps:new Date(formateDate)
        }
         let msgObj = new messageData(newObj2)
          savemessage = await msgObj.save()
@@ -21,10 +26,6 @@ exports.save = async function (data) {
                 value: false
             }
         }else{
-            var startDate = new Date();
-            var endDate = data.day+"T"+data.time
-            var formateDate = moment(endDate).utcOffset('+05:30').format('YYYY-MM-DD HH:mm:ss');
-            var seconds = (new Date(formateDate).getTime() - startDate.getTime());
             var intervel= setInterval(function timeStamps() {
                 let newObj = {
                     message: data.message
